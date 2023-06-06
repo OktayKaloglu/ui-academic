e61710866cfa9cbedd02db417bc6e497bdbd8c21
Oktay Kaloğlu <kaloglu.oktay@gmail.com>
2023-06-06 05:17:46 +0300

    instructor page v2

---
+++ components/cards/publication.js
@@ -0,0 +1,51 @@
+import { Card, Text } from "@nextui-org/react";
+
+function getCtitation(pub_id) {
+  if (pub_id) {
+    const id = pub_id.split(":");
+
+    const citationUrl =
+      "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=" +
+      id[0] +
+      "&citation_for_view=" +
+      pub_id;
+    window.open(citationUrl, "_blank");
+  }
+  return "#";
+}
+function displayDescription(publication, displayDesc) {
+  if (displayDesc) {
+    return (
+      <div>
+        <p>Publication Year: {publication.bib.pub_year}</p>
+        <p>Citation: {publication.bib.citation}</p>
+        <p>Number of Citations: {publication.num_citations}</p>
+      </div>
+    );
+  }
+
+  return <p></p>;
+}
+
+const PublicationCard = ({ publication, displayDesc }) => {
+  return (
+    <Card
+      css={{ marginTop: "1vh" }}
+      isHoverable
+      isPressable
+      onPress={() => getCtitation(publication.author_pub_id)}
+    >
+      <Card.Header>
+        <Text size={20}>{publication.bib.title}</Text>
+      </Card.Header>
+      <Card.Body>
+        <>{displayDescription(publication, displayDesc)}</>
+      </Card.Body>
+      <Card.Footer css={{ justifyContent: "flex-end" }}>
+        Read More...
+      </Card.Footer>
+    </Card>
+  );
+};
+
+export default PublicationCard;
--- pages/instructors.js
+++ pages/instructors.js
@@ -1,127 +1,119 @@
-import React from 'react';
-import instructorData from '../source/author.json';
-import { Image,Container, Card, Row, Text, Col, Spacer } from "@nextui-org/react";
- 
+import React from "react";
+import instructorData from "../source/author.json";
+import Link from "next/link";
+import {
+  Image,
+  Container,
+  Card,
+  Row,
+  Text,
+  Col,
+  Spacer,
+} from "@nextui-org/react";
+import PublicationCard from "@/components/cards/publication";
 
+const getMaxCitationsPublication = (publications) => {
+  let maxPublication = null;
+
+  for (const publication of publications) {
+    if (
+      !maxPublication ||
+      publication.num_citations > maxPublication.num_citations
+    ) {
+      maxPublication = publication;
+    }
+  }
+
+  return maxPublication;
+};
 const InstructorDetails = ({ instructor }) => {
+  const max_citation = getMaxCitationsPublication(instructor.publications);
   return (
-    <div>
-      <Container gap={0} display={ 'flex'} max-height = { '80%'} overflow= {'scroll'} >
-      <Row gap={1}
- >
-        <Col id='col1' css={{minHeight:'70vh'  }}>
-     
-          <Card css={{ $$cardColor: '$colors$primary' }}>
-          <Image
-               
-               autoResize="true"  
-               src={instructor.url_picture}
-               alt={instructor.name}
-               objectFit="cover"
-             />
+    <Container gap={0} display={"flex"} max-height={"80%"}>
+      <Row gap={1}>
+        <Col className="column">
+          <Card css={{ $$cardColor: "$colors$primary" }}>
+            <Card.Header>
+              <Text h1>{instructor.name}</Text>
+            </Card.Header>
+
             <Card.Body>
-            <h1>{instructor.name}</h1>
-           
-            
-            <p>Affiliation: {instructor.affiliation}</p>
-            <p>Email Domain: {instructor.email_domain}</p>
-             
+              <Row>
+                <Col xs>
+                  <Image
+                    autoResize="true"
+                    src={instructor.url_picture}
+                    alt={instructor.name}
+                    objectFit="contain"
+                  />
+                </Col>
+
+                <Col css={{ marginLeft: "2vh" }}>
+                  <Container>
+                    <Text size={20}>
+                      <b>Affiliation:</b> {instructor.affiliation}
+                    </Text>
+
+                    <Text size={20}>
+                      <b>Total Citation:</b> {instructor.citedby}
+                    </Text>
+                    <Text size={20}>
+                      <b>Top-cited publication:</b> {max_citation.num_citations}
+                    </Text>
+
+                    <PublicationCard
+                      publication={max_citation}
+                      displayDesc={false}
+                      index={"1"}
+                    />
+                  </Container>
+                </Col>
+              </Row>
             </Card.Body>
           </Card>
         </Col>
-        <Col overflow= {'scroll'}  >
-          <Card  css={{ $$cardColor: '$colors$primary'   }}>
-            <Card.Body >
-            <h2>Interests:</h2>
-            <ul>
-              {instructor.interests.map((interest, index) => (
-                <li key={index}>{interest}</li>
-              ))}
-            </ul>
+        <Col className="column">
+          <Card css={{ $$cardColor: "$colors$primary" }}>
+            <Card.Body>
+              <h2>Interests:</h2>
+              <ul>
+                {instructor.interests.map((interest, index) => (
+                  <li key={index}>{interest}</li>
+                ))}
+              </ul>
             </Card.Body>
-            </Card>
-            
-        </Col>
-        <Col css={{maxH:'70vh' , overflow:'auto' }}  >
-          <Container  >
-          <h2>Publications:</h2>
+            <Container>
+              <h2>Lessons:</h2>
 
-
-            
-              
-                {instructor.publications.map((publication, index) => (
-                  <Card key={index}>
-                    <Card.Body>
+              {instructor.publications.map((publication, index) => (
+                <Card css={{ marginTop: "1vh" }} isHoverable key={index}>
+                  <Card.Body>
                     <h3>{publication.bib.title}</h3>
                     <p>Publication Year: {publication.bib.pub_year}</p>
                     <p>Citation: {publication.bib.citation}</p>
                     <p>Number of Citations: {publication.num_citations}</p>
-                    <a href={publication.citedby_url}>Cited by</a>
-                    </Card.Body>
-                  </Card>
-                    
-                  
-                ))}
-              
-           
+                    <a>Read More...</a>
+                  </Card.Body>
+                </Card>
+              ))}
+            </Container>
+          </Card>
+        </Col>
+        <Col className="column">
+          <Container>
+            <h2>Publications:</h2>
+
+            {instructor.publications.map((publication, index) => (
+              <PublicationCard
+                key={index}
+                publication={publication}
+                displayDesc={true}
+              />
+            ))}
           </Container>
         </Col>
       </Row>
-      <Spacer y={1} />
-      <Row gap={1}>
-        <Col>
-          <Card css={{ $$cardColor: '$colors$primary' }}>
-            <Card.Body>
-              <Text h6 size={15} color="white" css={{ m: 0 }}>
-                1 of 3
-              </Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card css={{ $$cardColor: '$colors$primary' }}>
-            <Card.Body>
-              <Text h6 size={15} color="white" css={{ m: 0 }}>
-                2 of 3
-              </Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card css={{ $$cardColor: '$colors$primary' }}>
-            <Card.Body>
-              <Text h6 size={15} color="white" css={{ m: 0 }}>
-                3 of 3
-              </Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
     </Container>
-   
-      <h1>{instructor.name}</h1>
-      <img src={instructor.url_picture} alt={instructor.name} />
-      <p>Affiliation: {instructor.affiliation}</p>
-      <p>Email Domain: {instructor.email_domain}</p>
-      <h2>Interests:</h2>
-      <ul>
-        {instructor.interests.map((interest, index) => (
-          <li key={index}>{interest}</li>
-        ))}
-      </ul>
-      <h2>Publications:</h2>
-      <ul>
-        {instructor.publications.map((publication, index) => (
-          <li key={index}>
-            <h3>{publication.bib.title}</h3>
-            <p>Publication Year: {publication.bib.pub_year}</p>
-            <p>Citation: {publication.bib.citation}</p>
-            <p>Number of Citations: {publication.num_citations}</p>
-            <a href={publication.citedby_url}>Cited by</a>
-          </li>
-        ))}
-      </ul>
-    </div>
   );
 };
 
@@ -133,13 +125,11 @@ export async function getServerSideProps() {
   //for api
   //const response = await fetch('../source/author.json');
   //const instructor = await response.json();
-  const instructor=instructorData
-  
-    
- 
+  const instructor = instructorData;
+
   return {
     props: {
-      instructor
+      instructor,
     },
   };
 }
